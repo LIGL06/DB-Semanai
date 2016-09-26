d6990f034a57743446242fe5660538a410c2ff1f
Laboratorio de Interfaces <labinterfaces@iMac-de-Laboratorio.local>
2016-09-26 14:29:02 -0500

    json

---
+++ models/event.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongoUrl = 'mongodb://localhost:27017/semanai'
+
+mongoose.connect(mongoUrl, function(error){
+  if (error) {
+    throw error;
+  }
+  console.log('Conexión a BD...')
+})
+
+var email_match = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ , "Coloca un email válido "];
+
+var place_schema = new Schema({
+  idLugar: Number,
+  nombreLugar: String,
+  subnombreLugar: String,
+  descLugar: String,
+  historiaLugar: String,
+  tipicoLugar: String,
+  fotoLugar: String,
+  videoLugar: String,
+  coordenadasLugar: String,
+  estrellasLugar: {type: Number, min:[1,"Debe ser mayor a 1"],max:[5,"Debe ser menor a 5"]}
+})
+
+var user_schema = new Schema({
+  firstName: String,
+  lastName: String,
+username: {
+  type: String,
+  maxlength: [50, "Usuario muy grande"]
+  },
+email: {
+  type: String,
+  required: "El correo es obligatorio",
+  match: email_match
+  },
+})
+
+var comments = new Schema({
+  _creator: {type: Number, ref: 'User'},
+  title: String,
+})
+
+var User = mongoose.model('User',user_schema);
+var Place = mongoose.model('Place', place_schema);
+module.exports.Place = Place;
+module.exports.User = User;
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var stormpath = require('express-stormpath');
 var mongoose = require('mongoose');
+var multer = require('multer');
+var storage = multer.diskStorage({destination: 'uploads/',filename: function(req,file,cb){
+  cb(null, file.originalname + '-' + Date.now()+'.jpg')
+}});
+var upload = multer({storage:storage})
+var Place = require('../models/event').Place;
 var router = express.Router();
-mongoose.connect('mongodb://localhost/semanai');
 
 /* GET home page. */
 router.get('/',stormpath.loginRequired,function(req, res, next) {
@@ -17,8 +22,17 @@ router.get('/new',stormpath.loginRequired,function(req, res, next) {
   res.render('new', { title: 'Panel de Nuevo Evento' });
 });
 
-router.post('/new',stormpath.loginRequired,function(req, res, next){
-  res.send(req.body);
+router.post('/new',stormpath.loginRequired,upload.single('fotoLugar'),function(req, res, next){
+  var place = new Place({
+    idLugar: req.body.idLugar,
+    nombreLugar: req.body.nombreLugar,
+    subnombreLugar: req.body.subnombreLugar,
+    descLugar: req.body.descLugar,
+    historiaLugar: req.body.historiaLugar,
+    coordenadasLugar: req.body.coordenadasLugar,
+    fotoLugar: req.file.path
+  })
+  res.send(place);
 })
 
 router.get('/edit/#{id}',stormpath.loginRequired,function(req, res, next) {
