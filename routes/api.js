fe17e46c98420bbdc848cd263dae8be1a5b292c5
Laboratorio de Interfaces <labinterfaces@iMac-de-Laboratorio.local>
2016-09-30 15:10:08 -0500

    v14 30 sept

--- routes/api.js
+++ routes/api.js
@@ -113,6 +113,10 @@ router.post('/new/comment', function(req,res,next){
       }
     })
   })
+  res.setHeader('Access-Control-Allow-Origin','http://localhost:8100')
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,UPDATE')
+  res.setHeader('Access-Control-Allow-Headers','X-Requested-with,content-type')
+  res.setHeader('Access-Control-Allow-Cerenditials',false)
 })
 
 
